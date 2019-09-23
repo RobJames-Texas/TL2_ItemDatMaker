0704fb47217ab1b1fc9257dd53f9e1bfc821f4cf
Rob James <robjames.texas@yahoo.com>
2019-09-22 19:29:35 -0500

    Adding packages for code coverage.

---
+++ TL2_ItemDatMakerTests/run-coverage.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+dotnet test --logger "trx;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=BuildReports\Coverage\ /p:CoverletOutputFormat=cobertura /p:Threshold=10 /p:ThresholdStat=total
+
+FOR /F %%D IN ('dir /b /a:d /o:-n %userprofile%\.nuget\packages\reportgenerator\*') DO (
+    set directory=%%D
+    GOTO report
+)
+
+:report
+dotnet %userprofile%\.nuget\packages\reportgenerator\%directory%\tools\netcoreapp2.1\ReportGenerator.dll "-reports:BuildReports\Coverage\coverage.cobertura.xml" "-targetdir:BuildReports\Coverage" -reporttypes:HTML;HTMLSummary
+start BuildReports\Coverage\index.htm
